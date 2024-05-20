8e8b21411b30386b167b4df616ad1c6f596e53b4
Zainab Murtaza <162459926+Zainab090@users.noreply.github.com>
2024-05-20 23:19:26 +0500

    Assignment of Javascript chapter#5

---
+++ app.js
@@ -0,0 +1,117 @@
+
+// //Assignment#1
+let firstVariable=3;
+let secondVariable=5;
+let sum=firstVariable+secondVariable;
+document.write("Sum of 3 and 5 is "+sum+"<br>"); //Addition
+
+//Assignment#2
+let Variable1=3;
+let Variable2=5;
+let Multiply=Variable1*Variable2;
+document.write("Product of 3 and 5 is "+Multiply+"<br>");  //Multiplication
+let Variable3=3;
+let Variable4=5;
+let divide=Variable3/Variable4;
+document.write("Division of 3 and 5 is "+divide+"<br>");  //Division
+let Variable5=3;
+let Variable6=5;
+let subtract=Variable5-Variable6;
+document.write("Subtraction of 3 and 5 is "+subtract+"<br>");  //Subtraction
+let Variable7=3;
+let Variable8=5;
+let Modulus=Variable7%Variable8;
+document.write("Modulus of 3 and 5 is "+Modulus+"<br>");  //Modulus
+
+//Assignment#3
+let math_expression;
+document.write("Value after variable declaration is: ??.<br>");
+math_expression=5;
+document.write("Initial value:"+math_expression+".<br>");
+let newValue=++math_expression;
+document.write("Value after increment is:"+newValue+"<br>");
+let afterAdd=newValue+7;
+document.write("Value after addition is:"+afterAdd  +"<br>");
+let afterDecrement=--afterAdd;
+document.write("Value after decrement is:" +afterDecrement +"<br>");
+let afterModulus=afterDecrement%3;
+document.write("The remainder is :"+ afterModulus+"<br>");
+
+//Assignment#4
+let costOFOneTicket=600;
+let costOFFiveTicket=600*5;
+document.write("Total cost to buy 5 tickets to a movie is "+costOFFiveTicket+"PKR");
+
+//Assignment#5
+document.write("<h1>Table of 4</h1>");
+let number=4;
+for(let i = 1; i <= 10; i++) {
+    const result = i * number;
+    document.write(`${number} * ${i} = ${result}<br>`);
+}
+
+//Assignment#6
+let celsius=25;
+let formula= (celsius * 1.8) + 32;
+document.write(celsius+" celsius into fahrenheit is "+formula+"<br>");
+let fahrenheit=70;
+let formula2= (fahrenheit - 32) * 5/9;
+document.write(fahrenheit+" fahrenheit into celsius is"+formula2);
+
+//Assignment#7
+let priceOfItem1=650;
+document.write("<br>Price of item1 is "+priceOfItem1+"<br>");
+let quantityOfItem1=3;
+document.write("Quantity of item1 is "+quantityOfItem1+"<br>");
+const totalPriceOfItem1=quantityOfItem1*priceOfItem1;
+let priceOfItem2=100;
+document.write("Price of item2 is "+priceOfItem2+"<br>");
+let quantityOfItem2=7;
+document.write("Quantity of item2 is "+quantityOfItem2+"<br>");
+const totalPriceOfItem2=priceOfItem2*quantityOfItem2;
+let shippingCharges=100;
+document.write("Shiping Charges is "+shippingCharges+"<br>");
+const totalCharges=totalPriceOfItem1+totalPriceOfItem2+shippingCharges;
+document.write("Total cost of your order is "+totalCharges+"<br>");
+
+//Assignment#8
+let totalmarks=980;
+document.write("Total marks:"+totalmarks+"<br>");
+let marksobtained=804;
+document.write("marks obtained:"+marksobtained+"<br>");
+let percentage=marksobtained/totalmarks*100;
+document.write("Percentage:"+percentage+"%");
+
+//Assignment#9
+const US_dollar=10;
+const pkr=10*104.80;
+const Riyals=25;
+const pkcurrency=25*28;
+const pakistaniRupees=pkr+pkcurrency;
+document.write("<br>In pakistani currency "+pakistaniRupees);
+
+//Assignment#10                     
+let num=10;
+let addition=num+5;
+let multiplication=addition*10;
+let division=multiplication/2;
+document.write("<br>Answer is "+division);
+
+//Assignment#11
+let cuurent_year=2024;
+let birth_year=2006;
+let yourAge=
+
+
+
+
+
+
+
+
+
+
+
+
+
+
